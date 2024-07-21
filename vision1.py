32a6f0c290288fe90e9566122201c56a6a86d95f
Jasmine Soreng <jasmine.soreng@gmail.com>
2024-07-21 22:00:33 +0530

    Model update

--- vision1.py
+++ vision1.py
@@ -11,7 +11,7 @@ genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
 
 ## function to load Gemini pro vision model and get reponses
 
-model = genai.GenerativeModel("gemini-pro-vision")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def get_gemini_response(input,image):
     if input!="":
